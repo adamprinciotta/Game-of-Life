57229eca61a6802e602022313649ce0a6d2fe800
Adam Princiotta <adam.princiotta@habakkuk.gordon.edu>
2019-02-10 19:10:58 -0500

    first commit

--- Game-of-Life.cc
+++ Game-of-Life.cc
@@ -5,29 +5,21 @@ using std::cout;
 using std::cin;
 using std::endl;
 
-static const char ESC = 27; //at the start
-
-//cout << ESC << "[H" << ESC << "[J" << "Initial:" << endl; //just before displaying the initial board
-//cout << ESC << "[H" << "Generation " << some variable << ":" << endl; //just before displaying each board after the initial
-
-//cout << ESC << "[23;1H" << ESC << "[K"
-//<< "Press RETURN to continue";
-//while (cin.get() != ‘\n') {            // intentionally empty loop body   }
+static const char ESC = 27; 
 
 
-//while (cin.get() != '\n') { }
 Organism GameBoard::get(unsigned row, unsigned col){
 	return board_[row][col];
 }
 void GameBoard::set(unsigned row, unsigned col, Organism status){
 	board_[row][col]=status;
 }
-
+	
 
 void GameBoard::initialBoard(){
 	for(int i = 0; i < totalRows; i++){
 		for(int j = 0; j < totalCols; j++){
-			set(i,j,Organism::NONE);
+			set(i,j,	Organism::NONE);
 		}
 	}
 }
@@ -60,6 +52,93 @@ void GameBoard::makeBoard(){
 	cout << endl;
 }
 
+void GameBoard::checkNeigbors(){
+	int neighbors = 0;
+	for(int i = 1; i < totalRows - 1; i++){
+		for(int j = 1; j < totalCols - 1; j++){				
+			
+			if(get(i-1, j-1) == Organism::LIVING || get(i-1, j-1) == Organism::DYING)
+				neighbors++;
+
+			if(get(i, j-1) == Organism::LIVING || get(i, j-1) == Organism::DYING)
+				neighbors++;
+			
+			if(get(i+1, j-1) == Organism::LIVING || get(i+1, j-1) == Organism::DYING)
+				neighbors++;
+			
+			if(get(i-1, j) == Organism::LIVING || get(i-1, j) == Organism::DYING)
+				neighbors++;
+			
+			if(get(i-1, j+1) == Organism::LIVING || get(i-1, j+1) == Organism::DYING)
+				neighbors++;
+			
+			if(get(i+1, j) == Organism::LIVING || get(i+1, j) == Organism::DYING)
+				neighbors++;
+
+			if(get(i, j+1) == Organism::LIVING || get(i, j+1) == Organism::DYING)
+				neighbors++;
+			
+			if(get(i+1, j+1) == Organism::LIVING || get(i+1, j+1) == Organism::DYING)
+				neighbors++;
+
+			if ((neighbors < 2 || neighbors > 3) && (get(i, j) == Organism::LIVING))
+				set(i, j, Organism::DYING);
+			
+			else if (neighbors == 3 && get(i, j) == Organism::NONE)
+				set(i, j, Organism::GESTATING);
+				
+			neighbors = 0;
+			
+		}
+			
+	}		
+		
+}
+
+void GameBoard::updateBoard(int countGen){
+	cout << ESC << "[H" << "Generation " << countGen << ":" << endl;
+
+	for(int i = 1; i < totalRows - 1; i++){
+		for(int j = 1; j < totalCols - 1; j++){
+			if(get(i, j) == Organism::GESTATING)
+				set(i, j, Organism::LIVING);
+	
+			else if (get(i, j) == Organism::DYING)
+				set(i, j, Organism::NONE);
+		}
+	
+	}
+
+	for(int x = 0; x < totalCols; x++){
+		if(x == 0 || x == totalCols - 1)
+			cout << "+";
+		else	
+			cout << "-";
+	}
+					 
+	for(int i = 1; i < totalRows - 1; i++){
+		cout << "\n|";
+		for(int j = 1; j < totalCols - 1; j++){
+			if(get(i, j) == Organism::LIVING)
+				cout << "*";
+			else
+				cout << " ";
+		}
+		cout << "|";
+	} 
+	
+	cout << endl;
+	
+	for(int x = 0; x < totalCols; x++){
+		if(x == 0 || x == totalCols - 1)
+			cout << "+";
+		else	
+			cout << "-";
+	}
+	
+	cout << endl;
+		
+}
 
 int main(){
 	
@@ -68,6 +147,7 @@ int main(){
 
 	int initOrg;
 	int generations;
+	int countGen = 1;
 	int x;
 	int y;
 
@@ -80,18 +160,32 @@ int main(){
 	for(int i = 0; i < initOrg; i++){
 		cin >> x >> y;
 		cout << x << " " << y << endl;
-		Gboard.set(x + 1, y + 1, Organism::LIVING);
-
+		Gboard.set(x,y,Organism::LIVING);
 	}
 	
 	cout << "Generations?: ";
 	cin >> generations;
 	cout << endl;
 
+
+
 	Gboard.makeBoard();
-	//make updateBoard
+	
+	cout << ESC << "[23;1H" << ESC << "[K" << "Press RETURN to continue";
+	while (cin.get() != '\n') {            
+		//intentionally empty loop body
+	 }
+
+	while(countGen <= generations){
+		cout << ESC << "[23;1H" << ESC << "[K" << "Press RETURN to continue";
+		while (cin.get() != '\n') {            
+			//intentionally empty loop body
+	 	}	
+		Gboard.checkNeigbors();
+		Gboard.updateBoard(countGen);
+		countGen++;
+	} 
 	//check neighbors
 
 return 0;
-
 }
--- Makefile
+++ Makefile
@@ -3,7 +3,7 @@
 # Author (starting point): Russ Tuck
 # Authors (everything else):
 
-# Tictactoe uses C++ 2011.  Compile for debugging (-g) with warnings.
+# Game-of-Life uses C++ 2011.  Compile for debugging (-g) with warnings.
 # Google Test needs -pthread.
 CXXFLAGS=-std=c++11 -g -Wall -Wextra -pthread
 
@@ -15,10 +15,3 @@ all:		Game-of-Life \
 Game-of-Life :		Game-of-Life.o
 	$(CXX) $(CPPFLAGS) $(CXXFLAGS) -o $@ $^
 
-# Program to run tests and report results.
-#tictactoe_test:		tictactoe_test.o tictactoe.o
-#	$(CXX) $(CPPFLAGS) $(CXXFLAGS) -o $@ $^
-
-# Instructor's note: For now, we'll write and run tests with simple C++ code.
-# Later, we'll learn to use a unit test framework.  It will ease writing
-# tests and improve their output.
